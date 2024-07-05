48df46bd95e7b77ccd9d51e09f32a720838ba04f
wangshaojie05 <wangshaojie05@kuaishou.com>
2024-07-05 13:56:16 +0000

    add benchmark throughput

--- benchmarks/benchmark_throughput.sh
+++ benchmarks/benchmark_throughput.sh
@@ -1,14 +1,17 @@
 export VLLM_USE_FLASH_ATTN_TRITON=0 
 export PYTORCH_TUNABLEOP_ENABLED=1 
-export PYTORCH_TUNABLEOP_FILENAME=benchmarks/tuned.csv 
+export PYTORCH_TUNABLEOP_FILENAME=tuned.csv 
 export PYTORCH_TUNABLEOP_TUNING=0 
-export HIP_VISIBLE_DEVICES=2,3 
-python benchmarks/benchmark_throughput_cust.py \
-	--model /dockerx/models/Llama-2-70b-hf \
-	--num-prompts=1000 \
-	--input-len 1000 \
-	--output-len 500 \
+export HIP_VISIBLE_DEVICES=2,3
+export TOKENIZERS_PARALLELISM=false
+torchrun --standalone --nproc_per_node=2 \
+benchmark_throughput_cust.py \
+	--model ../../../models/Llama-2-70b-hf \
+	--num-prompts=10 \
+	--input-len 2048 \
+	--output-len 20 \
 	--tensor-parallel-size 2 \
 	--trust-remote-code \
-	--worker-use-ray
+	--dtype bfloat16 
+
 
